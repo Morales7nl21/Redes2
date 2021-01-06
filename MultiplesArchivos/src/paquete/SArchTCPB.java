9d43a56e16099c842a23fa3fba559d8ad8e3fbc8
= <=>
2021-01-06 05:15:13 -0600

    Ahora manda al otro servidor las imagenes

--- MultiplesArchivos/src/paquete/SArchTCPB.java
+++ MultiplesArchivos/src/paquete/SArchTCPB.java
@@ -9,104 +9,125 @@ public class SArchTCPB {
 
     public static void main(String[] args) {
         //boolean flagDir=true;
-        boolean flagEnvio = false;
-        int tamB, nagle;
+        boolean flagEnvio = false; //sin usar, nunca llega
+        int tamB, nagle, nArch;
 
-        ArrayList<String> nombresArchivos = new ArrayList<String>(); //Almacenara los nombres de los archivos que recibio para guardalros ahi
+        ArrayList<String> nombresArchivos = new ArrayList<>(); //Almacenara los nombres de los archivos que recibio para guardalros ahi
 
         try {
             //Creamos el socket
-            ServerSocket s = new ServerSocket(7000);
+            ServerSocket s = new ServerSocket(4000);
 
             //Iniciamos ciclo infinito del Servidor
             for (;;) {
-                //Esperamos una conexion
-                Socket cl = s.accept();
-                System.out.println("Conexion establecida desde: " + cl.getInetAddress() + " ; " + cl.getPort());
+                try ( //Esperamos una conexion
+                         Socket cl = s.accept()) {
+                    System.out.println("Conexion establecida desde: " + cl.getInetAddress() + " ; " + cl.getPort());
+                    try ( DataInputStream dis = new DataInputStream(cl.getInputStream())) {
+                        tamB = dis.readInt();
+                        System.out.println("BUFFER: " + tamB);
+                        nArch = dis.readInt();
+                        System.out.println("nARCH: " + nArch);
+                        //nagle = dis.readInt();
+                        //System.out.println("NAGLE: " + nagle);
+                        byte[] b = new byte[tamB];
+                        for (int i = 0; i < nArch; i++) {
+                            String nombre = dis.readUTF();
+                            nombresArchivos.add(nombre);
+                            System.out.println("Recibimos el archivo: " + nombre);
+                            long tam = dis.readLong();
+                            try ( DataOutputStream dos = new DataOutputStream(new FileOutputStream(nombre))) {
+                                long recibidos = 0;
+                                int n, porcentaje;
+                                long faltante;
+                                //Ciclo para recibir por partes el archivo
+                                while (recibidos < tam) {
+                                    faltante = tam - recibidos;
+                                    if (faltante < tamB) {
+                                        byte[] bf = new byte[(int) faltante];
+                                        n = dis.read(bf);
+                                        dos.write(bf, 0, n);
+                                        dos.flush();
+                                    } else {
+                                        n = dis.read(b);
+                                        dos.write(b, 0, n);
+                                        dos.flush();
+                                    }
+                                    recibidos += n;
+                                    porcentaje = (int) (recibidos * 100 / tam);
+                                    System.out.print("Recibido " + porcentaje + "%\r");
+                                }
+                                System.out.println("\n\nArchivo Recibido.");
+                                //nagle = dis.readInt();
+                                //System.out.println("NAGLE: " + nagle);                                
+                            }
 
-                DataInputStream dis = new DataInputStream(cl.getInputStream());
-                tamB = dis.readInt();
-
-                int nArch = dis.readInt();
-                nagle = dis.readInt();
-                byte[] b = new byte[tamB];
-                for (int i = 0; i < nArch; i++) {
-                    String nombre = dis.readUTF();
-                    nombresArchivos.add(nombre);
-                    System.out.println("Recibimos el archivo: " + nombre);
-                    long tam = dis.readLong();
-                    DataOutputStream dos = new DataOutputStream(new FileOutputStream(nombre));
-                    long recibidos = 0;
-                    int n, porcentaje;
-                    long faltante;
-                    //Ciclo para recibir por partes el archivo
-                    while (recibidos < tam) {
-                        faltante = tam - recibidos;
-                        if (faltante < tamB) {
-                            byte[] bf = new byte[(int) faltante];
-                            n = dis.read(bf);
-                            dos.write(bf, 0, n);
-                            dos.flush();
-                        } else {
-                            n = dis.read(b);
-                            dos.write(b, 0, n);
-                            dos.flush();
                         }
-                        recibidos += n;
-                        porcentaje = (int) (recibidos * 100 / tam);
-                        System.out.print("Recibido " + porcentaje + "%\r");
+                        flagEnvio = true;
+                        nagle = dis.readInt();
+                        System.out.println("NAGLE: " + nagle);
+
+                        if (flagEnvio) {
+                            //se procedera a enviar todo al otro vato
+                            try {
+                                Socket cl2 = new Socket("192.168.100.77", 9000);
+                                for (;;) {
+                                    try (DataOutputStream dose = new DataOutputStream(cl2.getOutputStream())) {
+                                        if (nagle == 0) {
+                                            cl2.setTcpNoDelay(true);
+                                        } else {
+                                            cl2.setTcpNoDelay(false);
+                                        }
+                                        dose.writeInt(tamB);//enviamos tamaño de buffer
+                                        dose.flush();
+                                        dose.writeInt(nombresArchivos.size());//enviamos numero de archivos
+                                        dose.flush();
+                                        
+                                        Iterator<String> archivo = nombresArchivos.iterator();
+                                        while (archivo.hasNext()) {
+                                            //Mediante archivo se comprobará el archivo en la carpeta para re enviarlo
+                                            String elemento = archivo.next();
+                                            File ficheroe = new File(elemento);
+                                            String archivoe = ficheroe.getAbsolutePath();//ruta
+                                            String nombree = ficheroe.getName();//nombre
+                                            long tame = ficheroe.length();//tamaño
+                                            System.out.println("paquete.SArchTCPB.main(): " + archivoe + " --- " + nombree + "---" + tame);
+                                            try (DataInputStream dise = new DataInputStream(new FileInputStream(archivoe))) {
+                                                dose.writeUTF(nombree);//enviamos nombre
+                                                dose.flush();
+                                                dose.writeLong(tame);//enviamos tamaño
+                                                dose.flush();
+                                                byte[] be = new byte[(int) tame];
+                                                long enviados = 0;
+                                                int porcentajee, ne;
+                                                while (enviados < tame) {
+                                                    ne = dise.read(be);
+                                                    dose.write(be, 0, ne);
+                                                    dose.flush();
+                                                    enviados += ne;
+                                                    porcentajee = (int) (enviados * 100 / tame);
+                                                    System.out.print("Enviado " + porcentajee + "%\r");
+                                                }
+                                            } //enviamos nombre
+                                            System.out.println("\n\nArchivo Enviado");
+                                            System.out.println(elemento);
+                                        }
+                                        dose.writeInt(nagle);
+                                        dose.flush();
+                                    }
+                                    cl2.close();
+                                }
+                            } catch (IOException e) {
+                                System.err.println(e);
+                            } finally {
+                                flagEnvio = false;
+                            }
+                        } //End if flag
+
                     }
-                    System.out.println("\n\nArchivo Recibido.");
-                    flagEnvio = true;
-                    dos.close();
-
                 }
-
-                dis.close();
-                cl.close();
-
-                if (flagEnvio) {
-                    //se procedera a enviar todo al otro vato
-                    try {
-                        /*
-                        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                        System.out.print("Escriba la direccion del servidor: ");
-                        String host = br.readLine();
-                        //System.out.print("\n\nEscriba el puerto: ");
-                        //int pto = Integer.parseInt(br.readLine());
-                        Socket cl2 = new Socket(host,7005);
-                        Iterator<String> archivo = nombresArchivos.iterator();
-                         */
-                        Socket cl2 = new Socket("192.168.100.77", 7005);
-
-                        DataOutputStream dos = new DataOutputStream(cl2.getOutputStream());
-                        if (nagle == 0) {
-                            cl2.setTcpNoDelay(true);
-                        } else {
-                            cl2.setTcpNoDelay(false);
-                        }
-                        
-                        dos.writeInt(tamB);//enviamos tamaño de buffer
-                        dos.flush();
-                        dos.writeInt(nagle);//enviamos nagle
-                        dos.flush();
-
-                        Iterator<String> archivo = nombresArchivos.iterator();
-                        while (archivo.hasNext()) {
-                            //Mediante archivo se comprobará el archivo en la carpeta para re enviarlo
-                            String elemento = archivo.next();
-                            System.out.println(elemento);
-                        }
-
-                    } catch (Exception e) {
-
-                    }
-
-                }
-
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
         }
     }
 }
