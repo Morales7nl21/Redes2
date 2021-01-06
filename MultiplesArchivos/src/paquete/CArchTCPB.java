3c1f2c343ad134398bf1aaa25c3a5b4f6e658bb2
= <=>
2021-01-05 22:51:18 -0600

    Se agrega multiples archivos

---
+++ MultiplesArchivos/src/paquete/CArchTCPB.java
@@ -0,0 +1,68 @@
+package paquete;
+
+import javax.swing.JFileChooser;
+import java.net.*;
+import java.io.*;
+import javax.swing.JOptionPane;
+
+public class CArchTCPB {
+    public static void main(String []args){
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Escriba la direccion del servidor: ");
+            String host = br.readLine();
+            System.out.print("\n\nEscriba el puerto: ");
+            int pto = Integer.parseInt(br.readLine());
+            Socket cl = new Socket(host,pto);
+            //boolean t = cl.getTcpNoDelay();
+            int tamB = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el tamaño para el buffer:"));
+            int Nagle = JOptionPane.showConfirmDialog(null,"¿Deseas activar el algoritmo de Nagle?"," ",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+            if(Nagle == 0){
+                cl.setTcpNoDelay(true);
+            }
+            else{
+                cl.setTcpNoDelay(false);
+            }
+            JFileChooser jf = new JFileChooser();
+            jf.setMultiSelectionEnabled(true);
+            int r = jf.showOpenDialog(null);
+            if(r == JFileChooser.APPROVE_OPTION){
+                File []f = jf.getSelectedFiles();
+                DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
+                dos.writeInt(tamB);//enviamos tamaño de buffer
+                dos.flush();
+                dos.writeInt(f.length);//enviamos numero de archivos
+                dos.flush();
+                //DataInputStream dis;
+                for(int i=0; i<f.length;i++){
+                    String archivo = f[i].getAbsolutePath();//ruta
+                    String nombre = f[i].getName();//nombre
+                    long tam = f[i].length();//tamaño
+                    DataInputStream dis = new DataInputStream(new FileInputStream(archivo));
+                    dos.writeUTF(nombre);//enviamos nombre
+                    dos.flush();
+                    dos.writeLong(tam);//enviamos tamaño
+                    dos.flush();
+                    byte[]b = new byte[(int)tam];
+                    long enviados = 0;
+                    int porcentaje,n;
+                    while(enviados < tam){
+                        n = dis.read(b);
+                        dos.write(b,0,n);
+                        dos.flush();
+                        enviados += n;
+                        porcentaje = (int)(enviados*100/tam);
+                        System.out.print("Enviado "+porcentaje+"%\r");
+                    }
+                    dis.close();
+                    System.out.println("\n\nArchivo Enviado");
+                }
+                //dis.close();
+                dos.close();
+                cl.close();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ MultiplesArchivos/src/paquete/SArchTCPB.java
@@ -0,0 +1,66 @@
+package paquete;
+
+import java.net.*;
+import java.io.*;
+
+public class SArchTCPB {
+    
+    public static void main(String []args){
+        boolean flagDir=true;
+        try{
+            //Creamos el socket
+            ServerSocket s = new ServerSocket(7000);
+            String user1;
+            //Iniciamos ciclo infinito del Servidor
+            for(;;){
+                //Esperamos una conexion
+                Socket cl = s.accept();
+                System.out.println("Conexion establecida desde: "+cl.getInetAddress()+" ; "+cl.getPort());
+               
+                DataInputStream dis = new DataInputStream(cl.getInputStream());
+                int tamB = dis.readInt();
+                int nArch = dis.readInt();
+                byte []b = new byte[tamB];
+                for(int i=0;i<nArch;i++){
+                    if (flagDir) {
+                                File directorio = new File("C:\\Users\\LENOVO 720\\Desktop\\IPN Documents\\6toSemestre\\Redes\\MultiplesArchivos\\Cliente_ServidorMArchivs\\Resources\\" + cl.getPort());
+                                directorio.mkdir();
+                                flagDir = false;
+                            }
+                    String nombre = dis.readUTF();
+                    System.out.println("Recibimos el archivo: "+nombre);
+                    long tam = dis.readLong();
+                    DataOutputStream dos = new DataOutputStream(new FileOutputStream(nombre));
+                    long recibidos = 0;
+                    int n, porcentaje;
+                    long faltante;
+                    //Ciclo para recibir por partes el archivo
+                    while(recibidos < tam){
+                        faltante = tam - recibidos;
+                        if(faltante < tamB){
+                            byte []bf = new byte[(int)faltante];
+                            n = dis.read(bf);
+                            dos.write(bf,0,n);
+                            dos.flush();
+                        }
+                        else{
+                            n = dis.read(b);
+                            dos.write(b,0,n);
+                            dos.flush();
+                        }
+                        recibidos += n;
+                        porcentaje = (int)(recibidos*100/tam);
+                        System.out.print("Recibido "+porcentaje+"%\r");
+                    }
+                    System.out.println("\n\nArchivo Recibido.");
+                    dos.close();
+                }
+                
+                dis.close();
+                cl.close();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
