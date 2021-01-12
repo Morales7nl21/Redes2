a91536007a3785857a6e15abaab5a6e38e544f62
AyonX12 <eugenioayona1im2@gmail.com>
2021-01-12 00:40:49 -0600

    Hola se hizo commit

--- proyectoREDMilton/RTCPpacket.java
+++ proyectoREDMilton/RTCPpacket.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.nio.*;
 
 // RR: Receiver Report RTCP Packet
-//cambio prueba
+
 //         0                   1                   2                   3
 //         0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
 //        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
---
+++ proyectoREDMilton/copiaControladora/CompiaControladorLog.java
@@ -0,0 +1,27 @@
+package copiaControladora;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.input.MouseEvent;
+
+public class CompiaControladorLog {
+
+    @FXML
+    private Hyperlink Resumen;
+
+    @FXML
+    private Button Entrar;
+
+    @FXML
+    void ConectaBD(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Ilumina(MouseEvent event) {
+
+    }
+
+}
